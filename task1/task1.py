213f51e0a98c79cf3c3dae50081822d5063e25ce
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2021-12-25 15:27:31 +0300

    Add files via upload

---
+++ task1/task1.py
@@ -0,0 +1,15 @@
+import sys
+a = []
+with open(sys.argv[1]) as inf:
+    for line in inf:
+        line = line.strip().split()
+        a.extend(line)
+A = [int(i) for i in a]
+i = 1
+result = []
+while True:
+    result.append(i)
+    i = 1 + (i + A[1] - 2) % A[0]
+    if i == 1:
+        break
+print(*result, sep='')
