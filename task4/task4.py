874f31d6facf3f311ec2a4ab81863a909bb4fbfc
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2021-12-25 15:29:08 +0300

    Add files via upload

---
+++ task4/task4.py
@@ -0,0 +1,10 @@
+import sys
+a = []
+with open(sys.argv[1]) as inf:
+    for line in inf:
+        line = line.strip()
+        a.append(line)
+A = [int(i) for i in a]
+count = 0
+m = sorted(A)[len(A) // 2]
+print(sum(abs(j - m) for j in A))
