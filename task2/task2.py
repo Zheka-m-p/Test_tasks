57d8c06b493911c11a3791e61e82a1d4e3ca526c
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2021-12-25 17:35:33 +0300

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,16 +1,19 @@
 import sys
+
 a = []
 with open(sys.argv[1]) as inf:
     for line in inf:
         line = line.strip().split()
         a.extend(line)
 A = [float(i) for i in a]
+
 b= []
 with open(sys.argv[2]) as inf:
     for line in inf:
         line= line.strip().split()
         b.extend(line)
 B = [float(i) for i in b]
+
 for i in range(0, len(B), 2):
     if ((A[0] - B[i])**2 + (A[1] - B[i + 1])**2) ** 0.5 == A[2]:
         print('0')
