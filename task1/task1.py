f307a2dc863e3e79333aa8e61547f37fb97a7496
Zheka-m-p <96652625+Zheka-m-p@users.noreply.github.com>
2021-12-25 17:26:37 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,10 +1,12 @@
 import sys
+
 a = []
 with open(sys.argv[1]) as inf:
     for line in inf:
         line = line.strip().split()
         a.extend(line)
 A = [int(i) for i in a]
+
 i = 1
 result = []
 while True:
@@ -12,4 +14,5 @@ while True:
     i = 1 + (i + A[1] - 2) % A[0]
     if i == 1:
         break
+        
 print(*result, sep='')
